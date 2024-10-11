1124fb4cf1f41dcd4a718cf2417771416429d082
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-10-11 17:29:56 +0530

    task 6 complete

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,9 +4,30 @@ let isValid = require('./auth_users.js').isValid
 let users = require('./auth_users.js').users
 const public_users = express.Router()
 
+const doesExist = (username) => {
+  let userswithsamename = users.filter((user) => user.username === username)
+
+  if (userswithsamename.length > 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
 public_users.post('/register', (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: 'Yet to be implemented' })
+  const username = req.body.username
+  const password = req.body.password
+
+  if (username && password) {
+    if (!doesExist(username)) {
+      users.push({ username: username, password: password })
+      return res.status(200).json({ message: 'User successfully registered' })
+    } else {
+      return res.status(404).json({ message: 'User already registered' })
+    }
+  }
+
+  return res.status(404).json({ message: 'Unable to register' })
 })
 
 // Get the book list available in the shop
